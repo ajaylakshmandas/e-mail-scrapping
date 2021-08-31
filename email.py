d9eab4525146db7c8039f665609267d0df265dea
Ajaylakshmandas <67698346+ajaylakshmandas@users.noreply.github.com>
2021-08-31 21:53:49 +0545

    123.py

--- email.py
+++ email.py
@@ -3,6 +3,8 @@ username = email[:email.index("@")]
 domain_name = email[email.index("@")+1:]
 format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
 print(format_)
+#ajay
+
 
 
 
